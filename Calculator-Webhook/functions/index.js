70584b3ec5852f3ed3cff283a954e8f6a6c3acdb
RWare12 <39044631+RWare12@users.noreply.github.com>
2018-06-04 15:55:54 +0800

    Update index.js

--- Calculator-Webhook/functions/index.js
+++ Calculator-Webhook/functions/index.js
@@ -1,63 +1,97 @@
+
+let firebase = require("firebase");
+let request = require('request');
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
-let firestore = admin.firestore();
+// let firestore = admin.firestore();
 
 
 // // Create and Deploy Your First Cloud Functions
+
+ // Initialize Firebase
+  let config = {
+    apiKey: "AIzaSyBcEalVGXZLsZqqgWcVSLNuqIzQk71y21U",
+    authDomain: "calculator-a3477.firebaseapp.com",
+    databaseURL: "https://calculator-a3477.firebaseio.com",
+    projectId: "calculator-a3477",
+    storageBucket: "calculator-a3477.appspot.com",
+    messagingSenderId: "1042922692923"
+  };
+  firebase.initializeApp(config);
+
+
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.webhook = functions.https.onRequest((request, response) => {
 
 
-    console.log("request.body.queryResult.Addition: ", request.body.queryResult.Addition);
+    console.log("request.body.queryResult: ", request.body.queryResult);
 
-
-    let query = request.body.queryResult.parameters;
-    let numList = query.numbers;
+    request('http://www.google.com', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    });
     
-    let bodyData = request.body;
+
+
+    /*let query = request.body.queryResult.parameters;
+    let numList = query.numbers;
+    let uid = firebase.database().ref().child('convo').push().key;
+
+    let bodyData = request.body.queryResult;
     let result;
     
     let ctr = 0;
     let firstNumberTemp = query.firstNumber;
     let secondNumberTemp = query.secondNumber;
 
+    //picking the structure of JSON
+    let user_message = request.body.originalDetectIntentRequest.payload.data.message.text;
+    let user_id = request.body.originalDetectIntentRequest.payload.data.sender.id;
+    let timestamp = request.body.originalDetectIntentRequest.payload.data.timestamp;
+    let userChatBot_id = request.body.originalDetectIntentRequest.payload.data.recipient.id;
+    let source = request.body.originalDetectIntentRequest.payload.source;
+    let fulfillmentText = request.body.queryResult.fulfillmentText;
+
+    //for storing the JSON above
+    let data = {};
+    //finalize JSON before pushing to realtime database
+    let updates = {};
+    
+
 
     switch(request.body.queryResult.action){
         
         case 'Add':
-            
             ctr = 0;
             result = 0;
             while (ctr < numList.length){
                 result += numList[ctr];
                 ctr++;
             }
-            
-
-            firestore.collection('rawData')
-            .add(bodyData)
-            .then(() => {
 
             response.send({
-                fulfillmentText:
-                    `and this is what we got, ${result}.`
-                });
 
-        
-            })
-            .catch((e => {
+                fulfillmentText: `and this is what we got, ${result}.`
 
-                console.log("error: ", e);
+            });
+            
+            //JSON that will be passed in realtime database
+            data = {
+                    user_id: user_id,
+                    user_message: user_message,
+                    timestamp: String(timestamp),
+                    userChatBot_id: userChatBot_id,
+                    chatBot_message: fulfillmentText,
+                    source: source
+            }
 
-                response.send({
-                    fulfillmentText:
-                        `something went wrong`
-                });
-
-            }))
+            updates['/convo/' + uid] = data;
+            firebase.database().ref().update(updates);
 
             break;
 
@@ -69,25 +103,25 @@ exports.webhook = functions.https.onRequest((request, response) => {
                 ctr++;
             }
 
-            firestore.collection('rawData')
-            .add(bodyData)
-            .then(() => {
+             response.send({
 
-            response.send({
-                fulfillmentText:
-                    `and this is what we got, ${result}.`
-                });
-            })
-            .catch((e => {
+                fulfillmentText: `and this is what we got, ${result}.`
 
-                console.log("error: ", e);
+            });
 
-                response.send({
-                    fulfillmentText:
-                        `something went wrong`
-                });
+             //JSON that will be passed in realtime database
+            data = {
+                    user_id: user_id,
+                    user_message: user_message,
+                    timestamp: timestamp,
+                    userChatBot_id: userChatBot_id,
+                    chatBot_message: fulfillmentText,
+                    source: source
+            }
 
-            }))
+            updates['/convo/' + uid] = data;
+            firebase.database().ref().update(updates);
+
             break;
 
         case 'Multiply':
@@ -98,25 +132,25 @@ exports.webhook = functions.https.onRequest((request, response) => {
                 ctr++;
             }
 
-            firestore.collection('rawData')
-            .add(bodyData)
-            .then(() => {
-
             response.send({
-                fulfillmentText:
-                    `and this is what we got, ${result}.`
-                });
-            })
-            .catch((e => {
 
-                console.log("error: ", e);
+                fulfillmentText: `and this is what we got, ${result}.`
 
-                response.send({
-                    fulfillmentText:
-                        `something went wrong`
-                });
+            });
+            
+            //JSON that will be passed in realtime database
+            data = {
+                    user_id: user_id,
+                    user_message: user_message,
+                    timestamp: timestamp,
+                    userChatBot_id: userChatBot_id,
+                    chatBot_message: fulfillmentText,
+                    source: source
+            }
 
-            }))
+            updates['/convo/' + uid] = data;
+            firebase.database().ref().update(updates);
+
             break;
 
         case 'Divide':
@@ -125,72 +159,95 @@ exports.webhook = functions.https.onRequest((request, response) => {
             result = 0;
             result = firstNumberTemp / secondNumberTemp;
 
-            
-            firestore.collection('rawData')
-            .add(bodyData)
-            .then(() => {
-
             response.send({
-                fulfillmentText:
-                    `and this is what we got, ${result}.`
-                });
-            })
-            .catch((e => {
 
-                console.log("error: ", e);
+                fulfillmentText: `and this is what we got, ${result}.`
 
-                response.send({
-                    fulfillmentText:
-                        `something went wrong`
-                });
+            });
+            
+            //JSON that will be passed in realtime database
+            data = {
+                    user_id: user_id,
+                    user_message: user_message,
+                    timestamp: timestamp,
+                    userChatBot_id: userChatBot_id,
+                    chatBot_message: fulfillmentText,
+                    source: source
+            }
 
-            }))
+            updates['/convo/' + uid] = data;
+            firebase.database().ref().update(updates);
+
+            break;
+
+        case 'input.unknown':
+            data = {
+                    user_id: user_id,
+                    user_message: user_message,
+                    timestamp: timestamp,
+                    userChatBot_id: userChatBot_id,
+                    chatBot_message: fulfillmentText,
+                    source: source
+            }
+
+            updates['/convo/' + uid] = data;
+            firebase.database().ref().update(updates);
+            
             break;
 
         case 'showInfo':
-            firestore.collection('rawData').get()
-                .then((querySnapShot) => {
+            // firestore.collection('rawData').get()
+            //     .then((querySnapShot) => {
 
-                    let showInfoFB = [];
-                    querySnapShot.forEach((doc) => { doc.push(doc.data()) });
+            //         let showInfoFB = [];
+            //         querySnapShot.forEach((doc) => { doc.push(doc.data()) });
 
-                    let fulfillmentText = `you have ${showInfoFB.length} data \n`;
+            //         let fulfillmentText = `you have ${showInfoFB.length} data \n`;
 
 
-                    showInfoFB.forEach((eachInfo, index) => {
+            //         showInfoFB.forEach((eachInfo, index) => {
 
-                        fulfillmentText += `index: ${index}, numList: ${eachInfo.numbers}`
-                    })
+            //             fulfillmentText += `index: ${index}, numList: ${eachInfo.numbers}`
+            //         })
 
-                    response.send({
+            //         response.send({
 
-                            fulfillmentText: fulfillmentText
-                    })
+            //                 fulfillmentText: fulfillmentText
+            //         })
 
-                })
-                .catch((err) => {
+            //     })
+            //     .catch((err) => {
 
-                    console.log('An error has occured: ' , err);
+            //         console.log('An error has occured: ' , err);
 
-                    response.send({
+            //         response.send({
 
-                        fulfillmentText: `something went wrong reading database`
-                    })
+            //             fulfillmentText: `something went wrong reading database`
+            //         })
 
-                })
-
+            //     })
 
             break;
 
-        default:
-                response.send({
+    }*/
 
-                    fulfillmentText: "something went wrong in reading database"
-                })
+});
 
+exports.insertIntoDB = functions.https.onRequest((request, response) => {
+    const text = request.query.text;
+    admin.database().ref('/test').push({text: text}).then(snapshot => {
 
-    }
+        response.redirect(303, snapshot.ref);
 
-    
+    })
+});
+
+exports.convertToUppercase = functions.database.ref('/test/{pushID}/text').onWrite(event => {
+
+    console.log('UpperCase function triggered');
+    const text = event.data.val();
+    console.log('text values: ', text);
+    const uppercaseText = text;
+    return event.data.ref.parent.child('uppercaseText').set(uppercaseText);
 
 });
